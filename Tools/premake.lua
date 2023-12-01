5d36133f0e2ecc190c548f8ed47ebe1503c84a13
Mr. Capybara <ruanlucaspgbr@outlook.com>
2023-11-30 22:29:59 -0400

    Small improve on premake file

--- Tools/premake.lua
+++ Tools/premake.lua
@@ -1,9 +1,21 @@
--- premake5.lua
 workspace "CutOfMeal"
-  configurations { "Debug", "Release" }
-  location "../Build"
+configurations { "Debug", "Release" }
+location "../Build"
+
+defines { "WORKSPACE" }
 
 project "Engine"
-  kind "ConsoleApp"
-  language "C++"
-  location "../Build/MyProject"
+kind "ConsoleApp"
+language "C++"
+location "../Build/Engine"
+
+local source_path = "../Build/"
+
+files {
+  source_path .. "*.h",
+  source_path .. "*.hpp",
+  source_path .. "*.c",
+  source_path .. "**.cpp"
+}
+
+defines { "ENGINE" }
